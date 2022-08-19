c0561ca469567283dfe678c8feae9d02db4a2459
Fabian Portilla <fas_456@hotmail.com>
2022-08-19 09:55:49 -0500

    Add new routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
 
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import { LoginPage } from '../auth/pages/LoginPage'
-import { DcPage } from '../heroes/pages/DcPage.jsx'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
-import { Navbar } from '../ui/components'
+import { LoginPage } from '../auth'
+import { HeroesRoutes } from '../heroes'
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
 
             <Routes>
-                <Route path="dc" element={<DcPage />} />
-                <Route path="marvel" element={<MarvelPage />} />
-
                 <Route path="login" element={<LoginPage />} />
-
-                <Route path="/*" element={<Navigate to="/login" />} />
-
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
         </>
 
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
             <Link
                 className="navbar-brand"
@@ -17,29 +17,45 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink
-                        className="nav-item nav-link"
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className="nav-item nav-link"
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/dc"
                     >
                         DC
                     </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/hero"
+                    >
+                        Hero
+                    </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink
-                        className="nav-item nav-link"
-                        to="/login"
+                    <span
+                        className="nav-item nav-link text-info"
+                    >
+                        Flportilla
+                    </span>
+                    <button
+                        className='nav-item nav-link btn'
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
